a6c7b04a890c34a171c491a7ed8483313296df90
admin <admin@example.ru>
2026-06-17 18:37:16 +0300

    test_webhook_app2-222222

--- apps/app2/app2.py
+++ apps/app2/app2.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from my App2!!!!!"
+    return "Hello from my App2!!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5023)
